73641ab0d2484ebbeb3938cb789f2a2ced6c67af
keryum <alejandro_suarezp@outlook.com>
2023-05-09 23:41:37 -0500

    avance hasta detección de colisiones

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ const game = canvas.getContext('2d');
 
 let canvasSize;
 let elementSize;
+let level = 0;
 
 const playerPosition = {
     x: undefined,
@@ -12,6 +13,8 @@ const giftPosition = {
     x: undefined,
     y: undefined,
 }
+
+let enemiesPositions = [];
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -38,10 +41,17 @@ function startGame() {
     game.font = elementSize + 'px Verdana';
     game.textAlign = '';
 
-    const map = maps[0];
+    const map = maps[level];
+
+    if (!map) {
+        gameWin();
+        return;
+    }
+
     const mapRows = map.trim().split('\n')
     const mapRowCols = mapRows.map(row => row.trim().split(''))
 
+    enemiesPositions = [];
     game.clearRect(0,0,canvasSize,canvasSize)
     
     mapRowCols.forEach((row, rowI) => {
@@ -58,6 +68,11 @@ function startGame() {
             } else if (col == 'I') {
                 giftPosition.x = posX;
                 giftPosition.y = posY;
+            } else if (col == 'X') {
+                enemiesPositions.push({
+                    x: posX,
+                    y: posY,
+                })
             }
 
             game.fillText(emoji, posX, posY)
@@ -67,12 +82,38 @@ function startGame() {
     movePlayer();
 }
 
+function levelWin() {
+    level++;
+    startGame()
+}
+
+function gameWin() {
+    console.log('Ganaste el juego siuuuuuuuuu')
+}
+
+function levelFail() {
+    playerPosition.x = undefined,
+    playerPosition.y = undefined,
+    startGame()
+}
+
 function movePlayer() {
-    const giftCollisionX = (playerPosition.x.toFixed(1) == giftPosition.x.toFixed(1)-6)
-    const giftCollisionY = ((playerPosition.y.toFixed(1)) == giftPosition.y.toFixed(1))
-    if (giftCollisionX && giftCollisionY) {
-        console.log('Chocaste')
+    const giftCollisionX = (playerPosition.x.toFixed(1) == giftPosition.x.toFixed(1)-6);
+    const giftCollisionY = ((playerPosition.y.toFixed(1)) == giftPosition.y.toFixed(1));
+    const giftCollision = giftCollisionX && giftCollisionY;
+    if (giftCollision) {
+        levelWin()
     }
+
+    const enemyCollision = enemiesPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(1)-6 == playerPosition.x.toFixed(1)
+        const enemyCollisionY = enemy.y.toFixed(1) == playerPosition.y.toFixed(1)
+        return enemyCollisionX && enemyCollisionY;
+    });
+    if(enemyCollision) {
+        levelFail()
+    }
+
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
